932f109360b760700ca2bf3d41cad42bef1d7825
Bienvenue-DANGNIGBE <bdangnigbe@gmail.com>
2026-01-05 23:55:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,48 +17,6 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropou
 from tensorflow.keras.utils import to_categorical
 
 
-# =================== Entraînement du modèle CNN ===================
-
-@st.cache_resource
-def train_model():
-    (X_train, y_train), (X_test, y_test) = mnist.load_data()
-
-    X_train = X_train / 255.0
-    X_test = X_test / 255.0
-
-    X_train = X_train.reshape(-1, 28, 28, 1)
-    X_test = X_test.reshape(-1, 28, 28, 1)
-
-    y_train = to_categorical(y_train, 10)
-    y_test = to_categorical(y_test, 10)
-
-    model = Sequential([
-        Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
-        MaxPooling2D(2,2),
-        Conv2D(64, (3,3), activation='relu'),
-        MaxPooling2D(2,2),
-        Dropout(0.25),
-        Flatten(),
-        Dense(128, activation='relu'),
-        Dropout(0.5),
-        Dense(10, activation='softmax')
-    ])
-
-    model.compile(
-        optimizer='adam',
-        loss='categorical_crossentropy',
-        metrics=['accuracy']
-    )
-
-    model.fit(X_train, y_train, epochs=8, batch_size=128, verbose=0)
-    accuracy = model.evaluate(X_test, y_test, verbose=0)[1]
-
-    return model, accuracy
-
-
-model, accuracy = train_model()
-
-
 # =================== Prétraitement de l'image ===================
 
 def preprocess_image(image):
